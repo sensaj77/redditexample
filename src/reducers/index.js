d22ba2a3e991873ad9d8af4fd348faea131aefb7
Michal Sawicki <michal@mobilewebpro.pl>
2016-06-03 16:08:53 +0200

    cleanup vol. 2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers }    from 'redux';
 import { routeReducer }       from 'redux-simple-router';
 
-import formDataReducer from './formData';
 import redditReducers from './redditReducers.js';
 
 export default combineReducers({
   routing: routeReducer,
-  formDataReducer: formDataReducer,
   redditReducers: redditReducers
 });
